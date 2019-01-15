4bd665f3942c6154bcabceeb750327eaa02b830c
Damon Collins <damon.c.p.collins@gmail.com>
2019-01-14 19:26:20 -0500

    done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 // use this to copy code snippets or use your browser's console
+function greet() {
+	console.log(`my name is ${this.name}, hi!`);
+}
+
+greet(); // my name is , hi!
+
+let person = {
+	name: 'Bob',
+	greet: greet
+};
+
+person.greet(); // my name is Bob, hi!
+let sally = { name: 'Sally' };
+
+function greet(customer) {
+	console.log(`Hi ${customer}, my name is ${this.name}!`);
+}
+
+let newGreet = greet.bind(sally); // newGreet is context-bound to sally
+
+newGreet('Bob');
+// Hi Bob, my name is Sally!
+
+greet('Bob');
+// Hi Bob, my name is !
+
+greet.bind(sally)('Bob');
